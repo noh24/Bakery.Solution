61268f543242952fdda6f7c804271b1b6f10e58b
Brian Noh <noh24@ymail.com>
2022-12-09 18:40:07 -0500

    Test GetTotalPrice with different quantities

--- Bakery.Tests/ModelTests/ChestnutBreadTests.cs
+++ Bakery.Tests/ModelTests/ChestnutBreadTests.cs
@@ -12,5 +12,23 @@ namespace Bakery.Tests
       ChestnutBread chestnutBread = new ChestnutBread(1);
       Assert.AreEqual(7, chestnutBread.GetTotalPrice());
     }
+    [TestMethod]
+    public void GetTotalPrice_PriceForTwoChestnutBread_Int()
+    {
+      ChestnutBread chestnutBread = new ChestnutBread(2);
+      Assert.AreEqual(14, chestnutBread.GetTotalPrice());
+    }
+    [TestMethod]
+    public void GetTotalPrice_PriceForThreeChestnutBread_Int()
+    {
+      ChestnutBread chestnutBread = new ChestnutBread(3);
+      Assert.AreEqual(16, chestnutBread.GetTotalPrice());
+    }
+    [TestMethod]
+    public void GetTotalPrice_PriceForSixChestnutBread_Int()
+    {
+      ChestnutBread chestnutBread = new ChestnutBread(6);
+      Assert.AreEqual(32, chestnutBread.GetTotalPrice());
+    }
   }
 }
