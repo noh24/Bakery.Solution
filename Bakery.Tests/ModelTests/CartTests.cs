2bef3943180c557b22f2cf8c2c46004515df319d
Brian Noh <noh24@ymail.com>
2022-12-09 17:21:29 -0500

    test get total price static method

--- Bakery.Tests/ModelTests/CartTests.cs
+++ Bakery.Tests/ModelTests/CartTests.cs
@@ -13,5 +13,14 @@ namespace Bakery.Tests
       Pastry pastry = new Pastry(1);
       Assert.AreEqual(2, Cart.AddItems(bread.Count, pastry.Count));
     }
+    [TestMethod]
+    public void GetTotalPrice_AddBreadAndPastryPrice_Int()
+    {
+      Bread bread = new Bread(1);
+      Pastry pastry = new Pastry(1);
+      bread.GetTotalPrice();
+      pastry.GetTotalPrice();
+      Assert.AreEqual(7, Cart.GetTotalPrice(bread.TotalPrice, pastry.TotalPrice));
+    }
   }
 }
