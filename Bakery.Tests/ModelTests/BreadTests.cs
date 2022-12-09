0a197bcd3791fa8c6226b9898222a6808062e881
Brian Noh <noh24@ymail.com>
2022-12-09 11:47:59 -0500

    rename file, namespace and class names

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTest
+  {
+    [TestMethod]
+    public void NameOfMethodWeAreTesting_DescriptionOfBehavior_ExpectedREturnValue()
+    {
+      // any necessary logic to prep for test. ex. instantiating an object
+      // Assert.AreEqual(EXPECTED RESULT, CODE TO TEST);
+    }
+  }
+}
