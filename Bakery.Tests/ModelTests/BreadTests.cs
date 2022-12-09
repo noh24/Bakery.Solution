ce5569b945d24aeb69ddf96bed614b5e9a7b063b
Brian Noh <noh24@ymail.com>
2022-12-09 13:45:53 -0500

    rename getprice to gettotalprice and add test for 4 bread

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -19,22 +19,29 @@ namespace Bakery.Tests
     public void GetTotalPrice_PriceForOneBread_Int()
     {
       Bread bread = new Bread(1);
-      bread.GetPrice();
+      bread.GetTotalPrice();
       Assert.AreEqual(5, bread.TotalPrice);
     }
     [TestMethod]
     public void GetTotalPrice_PriceForTwoBread_Int()
     {
       Bread bread = new Bread(2);
-      bread.GetPrice();
+      bread.GetTotalPrice();
       Assert.AreEqual(10, bread.TotalPrice);
     }
     [TestMethod]
     public void GetTotalPrice_PriceForThreeBread_Int()
     {
       Bread bread = new Bread(3);
-      bread.GetPrice();
+      bread.GetTotalPrice();
       Assert.AreEqual(10, bread.TotalPrice);
+    }
+    [TestMethod]
+    public void GetTotalPrice_PriceForFourBread_Int()
+    {
+      Bread bread = new Bread(4);
+      bread.GetTotalPrice();
+      Assert.AreEqual(15, bread.TotalPrice);
     }
   }
 }
