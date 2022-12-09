15eee0791a08ce5d25204193ca1188dd2c26336c
Brian Noh <noh24@ymail.com>
2022-12-09 17:21:48 -0500

    clear get total price test

--- Bakery/Models/Cart.cs
+++ Bakery/Models/Cart.cs
@@ -8,5 +8,9 @@ namespace Bakery.Models
     {
       return _totalItems = bread + pastry;
     }
+    public static int GetTotalPrice(int bread, int pastry)
+    {
+      return _totalPrice = bread + pastry;
+    }
   }
 }
