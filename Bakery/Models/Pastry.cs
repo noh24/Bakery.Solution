c34d9bd0b525c12f13246558b3d97435158a1251
Brian Noh <noh24@ymail.com>
2022-12-09 14:39:20 -0500

    add override GetTotalPrice() to clear test

--- Bakery/Models/Pastry.cs
+++ Bakery/Models/Pastry.cs
@@ -2,10 +2,12 @@ namespace Bakery.Models
 {
   public class Pastry : Cart
   {
-    // class methods goes here
     public Pastry(int count) : base(count)
     {
-      
+    }
+    public override int GetTotalPrice()
+    {
+      return TotalPrice = 2;
     }
   }
 }
