35b5895fa77927ef63f14c274152283c5219351e
Brian Noh <noh24@ymail.com>
2022-12-09 13:08:25 -0500

    add game plan in psuedocode

--- Bakery/Bakery.cs
+++ Bakery/Bakery.cs
@@ -1,9 +1,14 @@
-using Systems;
+using System;
 
 public class Program
 {
   public static void Main()
   {
-    //ui goes here
+    // welcome customer
+    // cost for bread and pastry
+    // customer how many of each
+    // return total price
+      // offer deals bread 1 = 5, 2 = 10, 3 = 10
+      // pastry : buy 1 = 2,  deal 3 = 5, 4 = 7, 5 = 9, 6 = 10 
   }
 }
