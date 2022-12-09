bcebfa0f321bab110dff321b618f0795f8dbf38e
Brian Noh <noh24@ymail.com>
2022-12-09 14:50:20 -0500

    refactor code to clear bread constructor price per unit property

--- Bakery/Models/Cart.cs
+++ Bakery/Models/Cart.cs
@@ -4,10 +4,12 @@ namespace Bakery.Models
   {
     public int Count {get; set;}
     public int TotalPrice {get; set;}
+    public int PricePerUnit {get; set;}
     public Cart(int count)
     {
       Count = count;
       TotalPrice = 0;
+      PricePerUnit = 0;
     }
     public abstract int GetTotalPrice();
   }
