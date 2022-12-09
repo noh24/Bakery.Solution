1277f5713835c59129fa77d648092d4899738b89
Brian Noh <noh24@ymail.com>
2022-12-09 14:55:36 -0500

    rearrange order of properties

--- Bakery/Models/Cart.cs
+++ Bakery/Models/Cart.cs
@@ -3,13 +3,13 @@ namespace Bakery.Models
   public abstract class Cart
   {
     public int Count {get; set;}
-    public int TotalPrice {get; set;}
     public int PricePerUnit {get; set;}
+    public int TotalPrice {get; set;}
     public Cart(int count)
     {
       Count = count;
-      TotalPrice = 0;
       PricePerUnit = 0;
+      TotalPrice = 0;
     }
     public abstract int GetTotalPrice();
   }
