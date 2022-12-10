efad9eeb3ad12f47cc8f0fa317c2819147bc31ff
Brian Noh <noh24@ymail.com>
2022-12-09 20:29:07 -0500

    refactor code to pass addtotalbread and pastry test

--- Bakery/Models/Cart.cs
+++ Bakery/Models/Cart.cs
@@ -2,11 +2,16 @@ namespace Bakery.Models
 {
   public class Cart
   {
-    private static int _totalItems {get; set;}
+    private static int _totalBread {get; set;}
+    private static int _totalPastry {get; set;}
     private static int _totalPrice {get; set;}
-    public static int AddItems(int bread, int pastry)
+    public static int AddTotalBread(int bread)
     {
-      return _totalItems = bread + pastry;
+      return _totalBread += bread;
+    }
+    public static int AddTotalPastry(int pastry)
+    {
+      return _totalPastry += pastry;
     }
     public static int GetTotalPrice(int bread, int pastry)
     {
